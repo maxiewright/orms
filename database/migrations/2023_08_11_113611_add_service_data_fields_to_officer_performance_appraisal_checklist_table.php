74d086adb3bedebd6ad976c3936b79667eab0a79
Maxie Wright <maxiewright@gmail.com>
2023-12-11 13:04:47 -0400

    add condition to ensure columns don't exsist in service data fields for perfromance appraisal checklist

--- database/migrations/2023_08_11_113611_add_service_data_fields_to_officer_performance_appraisal_checklist_table.php
+++ database/migrations/2023_08_11_113611_add_service_data_fields_to_officer_performance_appraisal_checklist_table.php
@@ -9,18 +9,25 @@ return new class extends Migration
     public function up()
     {
         Schema::table('officer_performance_appraisal_checklists', function (Blueprint $table) {
-            $table->foreignId('battalion_id')
-                ->after('appraisal_end_at')
-                ->nullable()
-                ->constrained();
-            $table->foreignId('rank_id')
-                ->after('battalion_id')
-                ->nullable()
-                ->constrained();
-            $table->tinyText('non_grading_reason')
-                ->after('officer_appraisal_grade_id')
-                ->nullable();
+            if (! Schema::hasColumn('officer_performance_appraisal_checklists', 'battalion_id')){
+                $table->foreignId('battalion_id')
+                    ->after('appraisal_end_at')
+                    ->nullable()
+                    ->constrained();
+            }
 
+            if(! Schema::hasColumn('officer_performance_appraisal_checklists', 'rank_id')){
+                $table->foreignId('rank_id')
+                    ->after('battalion_id')
+                    ->nullable()
+                    ->constrained();
+            }
+
+            if(! Schema::hasColumn('officer_performance_appraisal_checklists', 'non_grading_reason')){
+                $table->tinyText('non_grading_reason')
+                    ->after('officer_appraisal_grade_id')
+                    ->nullable();
+            }
         });
     }
 };
