b211a27eb2d1cddddad70809bf87fb6289c26d93
Maxie Wright <maxiewright@gmail.com>
2024-02-13 20:29:02 -0400

    add ServiceFund Module

---
+++ Modules/ServiceFund/Database/Seeders/ServiceFundDatabaseSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Modules\ServiceFund\Database\Seeders;
+
+use Illuminate\Database\Seeder;
+
+class ServiceFundDatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // $this->call([]);
+    }
+}
